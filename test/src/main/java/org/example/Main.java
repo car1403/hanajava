647b47f7e587549f97cf0217f8882690cee6975b
User <jmlee@tonesol.com>
2024-03-11 12:41:55 +0900

    240311

--- test/src/main/java/org/example/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-
-        Cust c = Cust.builder().a(100).str("").build();
-        System.out.println(c.getA());
-    }
-}
