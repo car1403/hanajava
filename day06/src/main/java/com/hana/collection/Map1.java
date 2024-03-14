c0cf733b6b33299e2f165556fe45d6d095b57f9b
User <jmlee@tonesol.com>
2024-03-14 13:38:54 +0900

    240314

--- day06/src/main/java/com/hana/collection/Map1.java
+++ day06/src/main/java/com/hana/collection/Map1.java
@@ -1,9 +1,28 @@
 package com.hana.collection;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class Map1 {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+
+        Map<String,String> map = new HashMap<>();
+        map.put("k1", "v1");
+        map.put("k2", "v2");
+        map.put("k3", "v3");
+        map.put("k3", "v33");
+
+        System.out.println(map.get("k2"));
+        Set<String> keys = map.keySet();
+
+        keys.forEach((k)->System.out.println(k));
+
+        Collection<String> col = map.values();
+        col.forEach((v)->System.out.println(v));
+
     }
 
 }
