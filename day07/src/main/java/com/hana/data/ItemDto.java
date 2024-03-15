619f6fff8a6fb92eed7500b8f406a1875475c832
User <jmlee@tonesol.com>
2024-03-15 17:46:07 +0900

    240315

---
+++ day07/src/main/java/com/hana/data/ItemDto.java
@@ -0,0 +1,12 @@
+package com.hana.data;
+
+import java.util.Date;
+
+public class ItemDto {
+    private int id;
+    private String name;
+    private long price;
+    private Date regdate;
+    private Date updatedate;
+
+}
--- day07/src/main/java/com/hana/frame/SQL.java
+++ day07/src/main/java/com/hana/frame/SQL.java
@@ -1,5 +1,18 @@
 package com.hana.frame;
 
 public class SQL {
+    // Cust SQL
     public static String insertCust = "INSERT INTO db_cust VALUES (?,?,?)";
+
+    // Item SQL
+    public static String insertItem =
+            "INSERT INTO db_item VALUES (0,?,?,NOW(),NOW())";
+    public static String updateItem =
+            "UPDATE db_item SET name=?, price=?, updatedate=NOW() WHERE id=?";
+    public static String deleteItem =
+            "DELETE FROM db_item WHERE id = ?";
+    public static String selectAllItem =
+            "SELECT * FROM db_item ORDER BY id";
+    public static String selectOneItem =
+            "SELECT * FROM db_item WHERE id = ?";
 }
